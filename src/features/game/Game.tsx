cb78fc603e57e4541a9806d9e95d31e0ba8120ef
marcohefti <marco@heftiweb.ch>
2022-12-01 14:21:01 +0700

    running sound integration

--- src/features/game/Game.tsx
+++ src/features/game/Game.tsx
@@ -1,11 +1,14 @@
 import React, { Fragment } from "react";
 import { Start } from "../start/Start";
+import { useAppSelector } from "../../app/hooks";
+import { GAME_RUNNING, GAME_START, selectGame } from "./gameSlice";
+import Running from "../running/Running";
 
 function Game() {
-
-
+    const { mode } = useAppSelector(selectGame);
     return <Fragment>
-        <Start/>
+        {mode === GAME_START && <Start/>}
+        {mode === GAME_RUNNING && <Running/>}
     </Fragment>;
 }
 
---
+++ src/features/running/Running.tsx
@@ -0,0 +1,20 @@
+import React, { Fragment } from "react";
+import { Typography } from "@mui/material";
+import useSound from "use-sound";
+import kick from "./KICK01.wav";
+
+function Running() {
+    const [play] = useSound(kick);
+    setInterval(() => {
+        play();
+        console.log("kick");
+    }, 500);
+
+    return <Fragment>
+        <Typography align={"center"} variant={"h5"} component={"h2"}>
+            Match the beat
+        </Typography>
+    </Fragment>;
+}
+
+export default Running;
