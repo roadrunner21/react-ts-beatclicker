2f8e926851efb767f6ddfc68629dce7788766410
marcohefti <marco@heftiweb.ch>
2022-12-13 13:36:04 +0700

    implement persistent store

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,11 +2,22 @@ import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
 import gameReducer from "../features/game/gameSlice";
 import settingsReducer from "../features/settings/settingsSlice";
 
+let preloadedState = {};
+const item = localStorage.getItem("reduxState");
+if (item !== null) {
+    preloadedState = JSON.parse(item);
+}
+
 export const store = configureStore({
     reducer: {
         game: gameReducer,
         settings: settingsReducer,
     },
+    preloadedState,
+});
+
+store.subscribe(() => {
+    localStorage.setItem("reduxState", JSON.stringify(store.getState()));
 });
 
 export type AppDispatch = typeof store.dispatch;
