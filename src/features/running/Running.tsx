801963863db27b0e3edf1d8735f3ce29fe09b399
marcohefti <marco@heftiweb.ch>
2022-12-15 01:56:09 +0700

    add phases to the runtime

--- src/features/running/Running.tsx
+++ src/features/running/Running.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { Typography } from "@mui/material";
 import { useAppSelector } from "../../app/hooks";
 import { selectSettings } from "../settings/settingsSlice";
@@ -12,17 +12,41 @@ export interface RunningProps {
 function Running(props: RunningProps) {
     const { bpm } = useAppSelector(selectSettings);
     const { play, setVolume } = props;
+    const [text, setText] = useState("Get ready...");
     // using a ref to ensure useEffect gameLoop hook only runs once even in strictMode
     const effectCalled = useRef(false);
     // Use the useCallback hook to avoid re-creating the function on every render
     const gameLoop = useCallback(() => {
         let i = 0;
 
+        // time axis
         function tick() {
             play();
-            if (i < 5) {
-                setVolume(0.2 + i / 5);
+
+            switch (true) {
+                case i < 5:
+                    // Increase volume from 0.2 to 1.0 during the first five iterations
+                    setVolume(0.2 + i / 5);
+                    break;
+                case i < 10:
+                    setText("Beat is going to drop out");
+                    break;
+                case i < 15:
+                    // Decrease volume from 1.0 to 0.0 during the next five iterations
+                    setVolume(1.0 - (i - 9) / 5);
+                    break;
+                case i < 25:
+                    setText("Let's go!");
+                    break;
+                case i < 35:
+                    setText("Keep going!");
+                    // start recording user input for the next 20 iterations
+                    break;
+                default:
+                    setText("Finish");
+                    return;
             }
+
             i++;
             setTimeout(tick, 60000 / bpm);
         }
@@ -43,11 +67,11 @@ function Running(props: RunningProps) {
             <Typography align={"center"} variant={"h5"} component={"h2"}>
                 Match the beat
             </Typography>
+            <Typography align={"center"} component={"p"}>
+                {text}
+            </Typography>
         </>
     );
 }
 
 export default Running;
-
-
-
