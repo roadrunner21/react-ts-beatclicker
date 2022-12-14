eed2baf0a7eb739c59d270678c63cd760710288c
marcohefti <marco@heftiweb.ch>
2022-12-14 20:29:46 +0700

    optimize the gameloop and fix a variety of bugs pertaining to double rendering issues related to React.strictMode

--- src/features/game/Game.tsx
+++ src/features/game/Game.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Start from "../start/Start";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
-import { GAME_LOADING, GAME_RUNNING, GAME_READY, selectGame, setMode } from "./gameSlice";
+import { GAME_LOADING, GAME_READY, GAME_RUNNING, selectGame, setMode } from "./gameSlice";
 import Running from "../running/Running";
 import { Box } from "@mui/material";
 import useSound from "use-sound";
@@ -9,16 +9,21 @@ import kick from "../running/KICK01.wav";
 
 function Game() {
     const dispatch = useAppDispatch();
+    const [volume, setVolume] = useState(0.2);
     const onLoad = () => {
         dispatch(setMode(GAME_READY));
     };
     // preload the sound file
-    const [play] = useSound(kick, { onload: onLoad });
+    const [play] = useSound(kick, {
+        volume,
+        interrupt: false,
+        onload: onLoad,
+    });
 
     const { mode } = useAppSelector(selectGame);
     return <Box pt={10}>
         {(mode === GAME_LOADING || mode === GAME_READY) && <Start/>}
-        {mode === GAME_RUNNING && <Running play={play}/>}
+        {mode === GAME_RUNNING && <Running play={play} setVolume={setVolume}/>}
     </Box>;
 }
 
--- src/features/running/Running.tsx
+++ src/features/running/Running.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import { Typography } from "@mui/material";
 import { useAppSelector } from "../../app/hooks";
 import { selectSettings } from "../settings/settingsSlice";
@@ -6,21 +6,48 @@ import { PlayFunction } from "use-sound/dist/types";
 
 export interface RunningProps {
     play: PlayFunction;
+    setVolume: (volume: number) => void;
 }
 
 function Running(props: RunningProps) {
     const { bpm } = useAppSelector(selectSettings);
-    const { play } = props;
+    const { play, setVolume } = props;
+    // using a ref to ensure useEffect gameLoop hook only runs once even in strictMode
+    const effectCalled = useRef(false);
+    // Use the useCallback hook to avoid re-creating the function on every render
+    const gameLoop = useCallback(() => {
+        let i = 0;
 
-    setInterval(() => {
-        play();
-    }, 60000 / bpm);
+        function tick() {
+            play();
+            if (i < 5) {
+                setVolume(0.2 + i / 5);
+            }
+            i++;
+            setTimeout(tick, 60000 / bpm);
+        }
 
-    return <>
-        <Typography align={"center"} variant={"h5"} component={"h2"}>
-            Match the beat
-        </Typography>
-    </>;
+        tick();
+    }, [play, setVolume, bpm]);
+
+    useEffect(() => {
+        if (effectCalled.current) {
+            return;
+        }
+        gameLoop();
+        effectCalled.current = true;
+    }, [gameLoop]);
+
+    return (
+        <>
+            <Typography align={"center"} variant={"h5"} component={"h2"}>
+                Match the beat
+            </Typography>
+        </>
+    );
 }
 
 export default Running;
+
+
+
