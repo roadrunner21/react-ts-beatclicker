d677ffd14d57869d1c1cda674567ca00e3f9de5c
marcohefti <marco@heftiweb.ch>
2022-12-22 12:06:07 +0700

    jest support and implementation for About component

---
+++ src/features/about/About.spec.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import About from "./About";
+
+describe("About component", () => {
+    test("renders headline", () => {
+        render(<About/>);
+        expect(screen.getByText(`About Beat Clicker`)).toBeInTheDocument();
+    });
+
+    test("renders score algorithm section", () => {
+        render(<About/>);
+        expect(screen.getByText("Score Algorithm")).toBeInTheDocument();
+        expect(screen.getByText("Differences are cumulative", { exact: false })).toBeInTheDocument();
+    });
+
+    test("renders technology section", () => {
+        render(<About/>);
+        expect(screen.getByText("Technology")).toBeInTheDocument();
+        expect(screen.getByText("React and Redux", { exact: false })).toBeInTheDocument();
+    });
+
+    test("renders infrastructure section", () => {
+        render(<About/>);
+        expect(screen.getByText("Infrastructure")).toBeInTheDocument();
+        expect(screen.getByText("/pages", { exact: false })).toBeInTheDocument();
+    });
+});
